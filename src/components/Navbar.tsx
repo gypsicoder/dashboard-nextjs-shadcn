042224b2bd3b09827075ae569ad8b9e6e11009ae
Bishnu Pada Chanda <>
2026-01-02 15:47:09 +0100

    Added Light and Dark theme support

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,65 @@
+"use client"
+
 import Link from "next/link"
-import { Moon, User, Settings, LogOut } from "lucide-react"
+import { Moon, User, Settings, LogOut, Sun } from "lucide-react"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { Button } from "./ui/button"
+import { useTheme } from "next-themes"
 
 const Navbar = () => {
-  return (
-    <nav className='flex items-center justify-between p-4'>
-        CollapseButton
-        <div className="flex items-center gap-4">
-            <Link href="/">Dashboard</Link>
-            <Moon />
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <Avatar>
-                        <AvatarImage src="https://avatars.githubusercontent.com/u/1951794" />
-                        <AvatarFallback>BC</AvatarFallback>
-                    </Avatar>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent sideOffset={10}>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                        <User className="w-[1.3rem] h-[1.3rem] mr-2"/>Profile
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Settings className="w-[1.3rem] h-[1.3rem] mr-2"/>Settings
-                    </DropdownMenuItem>
-                    <DropdownMenuItem variant="destructive">
-                        <LogOut className="w-[1.3rem] h-[1.3rem] mr-2"/>Logout
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </div>
-    </nav>
-  )
+
+    const { setTheme } = useTheme()
+
+    return (
+        <nav className='flex items-center justify-between p-4'>
+            CollapseButton
+            <div className="flex items-center gap-4">
+                <Link href="/">Dashboard</Link>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon">
+                        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                        <span className="sr-only">Toggle theme</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => setTheme("light")}>
+                        Light
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        Dark
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("system")}>
+                        System
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                    </DropdownMenu>
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://avatars.githubusercontent.com/u/1951794" />
+                            <AvatarFallback>BC</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent sideOffset={10}>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            <User className="w-[1.3rem] h-[1.3rem] mr-2"/>Profile
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Settings className="w-[1.3rem] h-[1.3rem] mr-2"/>Settings
+                        </DropdownMenuItem>
+                        <DropdownMenuItem variant="destructive">
+                            <LogOut className="w-[1.3rem] h-[1.3rem] mr-2"/>Logout
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
